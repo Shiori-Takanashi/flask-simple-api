0701ead98a5d51e5269eb23d880da8ddc9805a26
Takanashi <nao69alter@gmail.com>
2025-11-29 13:27:30 +0900

    tests

---
+++ src/flask_simple_api/api/routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify
+from .hello import get_hello
+from .time import get_time
+
+api_bp = Blueprint("api", __name__)
+
+
+@api_bp.route("/hello-json")
+def hello_page() -> None:
+    return jsonify(get_hello())
+
+
+@api_bp.route("/time-json")
+def time_page():
+    return jsonify(get_time())
